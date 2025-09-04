5404361edd14eb045d5811f7d807537458f208be
kunal1029 <kunal.shivhare0529@gmail.com>
2025-09-04 15:52:05 +0530

    UID

--- Backend/helper/firebase.js
+++ Backend/helper/firebase.js
@@ -1,13 +1,20 @@
 //.mjs
 import admin from "firebase-admin";
+import dotenv from "dotenv";
+dotenv.config();
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
-const serviceAccount = require("./serviceAccountKey.json"); 
+// const serviceAccount = require("./serviceAccountKey.json"); 
 if (!admin.apps.length) {
+  const serviceAccount = JSON.parse(process.env.FIREBASE);
+
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
 }
+
+
+// console.log("ks",process.env.FIREBASE) //undefined
 
 export const adminAuth = admin.auth(); // Optional
 export default admin;
